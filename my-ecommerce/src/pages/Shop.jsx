afb76b99e8f1a447554f510750cf701f2781473d
Halil Sak <hallsak55@gmail.com>
2024-08-20 12:33:33 +0300

    shop page created

---
+++ my-ecommerce/src/pages/Shop.jsx
@@ -0,0 +1,11 @@
+import Header from "../layout/Header";
+import Footer from "../layout/Footer";
+
+export default function HomePage() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
